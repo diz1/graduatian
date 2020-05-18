7dfc68005ca0c4215e648a895aa2bda5e9aa8bb4
diz1 <dima-smirnov1@mail.ru>
2020-05-18 12:02:04 +0300

    Fix :)

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,11 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			// {
-			// 	test: /\.js$/,
-			// 	loader: 'babel-loader',
-			// 	exclude: '/node_modules'
-			// },
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: '/node_modules'
+			},
 			{
 				test: /\.css$/i,
 				use: ['style-loader', 'css-loader'],
